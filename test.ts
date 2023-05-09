ea5de702a9698fb9e01c0881db7ace8f637b2174
unknown <leishen@null-lab.com>
2023-05-09 14:48:13 +0800

    add test

--- test.ts
+++ test.ts
@@ -1,1 +1,7 @@
 // tests go here; this will not be compiled when this package is used as an extension.
+BMP280.em_initBmp280()
+BMP280.em_bmp280_Address(BMP280_I2C_ADDRESS.ADDR_0x76)
+basic.forever(function () {
+    basic.showString("" + (BMP280.em_pressure()))
+    basic.showString("" + (BMP280.em_temperature()))
+})
